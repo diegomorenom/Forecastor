27c448c173b7f8c381c4eb0d02bbac7c43907947
diegoem93 <diegoem93@gmail.com>
2024-02-26 22:17:39 -0800

    API developed

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
+from typing import List, Dict
+import yaml
 from forecast_generator import forecast_process as fp
 
 import json
 import os
 import sys
-import itertools
-from tqdm import tqdm
-from time import sleep
+
+import pandas as pd
 
 import warnings
 warnings.filterwarnings('ignore')
-#import pprint
 
 path = os.getcwd()
 parent_path = os.path.abspath(os.path.join(path, os.pardir))
@@ -20,20 +22,43 @@ sys.path.append(data_path)
 sys.path.append(forecast_path)
 
 
-from data_handler import get_data
+app = FastAPI()
 
-# Load JSON file
-with open("parameters.json", "r") as file:
-    info = json.load(file)
+def read_csv(file: UploadFile) -> pd.DataFrame:
+    """
+    Read CSV file and return DataFrame
+    """
+    df = pd.read_csv(file.file)
+    return df
 
-# Prepare your data, models, parameters, and forecast_days
-data = get_data()
-models = ['HoltWinters']#info[0]["Models"]
-forecast_days = info[0]["ForecastDays"]
-parameters = info[1]
+def parse_yaml(yaml_data):
+    try:
+        parsed_data = yaml.safe_load(yaml_data)
+        return parsed_data
+    except yaml.YAMLError as e:
+        return {"error": "Invalid YAML format", "detail": str(e)}
 
-# Instantiate a class that inherits from BaseForecastingProcess
-forecast_process_instance = fp.ForecastingProcess(data, models, parameters, forecast_days)
+@app.post("/run_forecasting/")
+async def run_forecasting(
+    json_file: UploadFile = File(...), 
+    csv_file: UploadFile = File(...)):
+    # Read JSON file
+    json_data = await json_file.read()
+    parsed_json = json.loads(json_data)
+    
+    # Read CSV file
+    data = pd.read_csv(csv_file.file)
+    
+    # Process data here using parsed_json and df
+    models = ['HoltWinters']#parsed_json[0]["Models"]
+    forecast_days = parsed_json[0]["ForecastDays"]
+    parameters = parsed_json[1]
 
-# Call the run_all_models method
-forecast_process_instance.run_all_models()
+    # Instantiate ForecastingProcess class
+    forecast_process_instance = fp.ForecastingProcess(data, models, parameters, forecast_days)
+
+    # Call run_all_models method
+    forecast_process_instance.run_all_models()
+
+    return {"message": "Forecasting process completed successfully."}
+    
